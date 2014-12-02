0e9a18a1b54e22f9964745a880ca283fd6ba4c9f
Juanmihd <juanmihd@gmail.com>
2014-12-02 08:55:11 +0000

    Complete the generation of blocks. Adding how to rebuild the mesh from the blocks

--- lsystem_mesh.h
+++ lsystem_mesh.h
@@ -117,31 +117,40 @@ namespace octet{
               new_block = new Block();
               printf("Stack pos:\n");
               printf_mat4t(back_stack->pos);
-              back_stack->pos.multMatrix(back_stack->transform);
+              printf("Stack transform:\n");
+              printf_mat4t(back_stack->transform);
+              //Fill new block
               new_block->pos = back_stack->pos;
               new_block->transform = back_stack->transform;
+              new_block->radio = back_stack->radio;
+              //Add new block
+              blocks[iteration].push_back(new_block);
+              //Update turtle3d
+              back_stack->pos.multMatrix(back_stack->transform);
+              //Debuging things
               printf("New block pos:\n");
               printf_mat4t(new_block->pos);
               printf("New block transform:\n");
               printf_mat4t(new_block->transform);
-              new_block->radio = back_stack->radio;
-              blocks[iteration].push_back(new_block);
               break;
             case '[':
+              //Reserve new block
               back_stack = new Block();
-              back_stack->pos = blocks[iteration].back()->pos;
+              //Copy the block
+              back_stack->pos = stack3d.back()->pos;
               back_stack->transform = stack3d.back()->transform;
               back_stack->radio = stack3d.back()->radio;
-              back_stack->pos.multMatrix(back_stack->transform);
+              //Add to stack of 3dTurtle
+              stack3d.push_back(back_stack);
+              back_stack = stack3d.back();
               printf("Stack pos:\n");
               printf_mat4t(back_stack->pos);
               printf("Stack transform:\n");
               printf_mat4t(back_stack->transform);
-              stack3d.push_back(back_stack);
-              back_stack = stack3d.back();
               break;
             case ']':
               stack3d.pop_back();
+              back_stack = stack3d.back();
               break;
             case '+':
               back_stack->transform.rotateX(angle_X);
@@ -158,20 +167,46 @@ namespace octet{
       void update(){
         //If the tree is not up to date
         if (ls_generated[cur_iteration] != _GENERATED){
+          ls_generated[cur_iteration] = _GENERATED;
           gl_resource::wolock vlock(get_vertices());
           vertex *vtx = (vertex*)vlock.u8();
           gl_resource::wolock ilock(get_indices());
           uint32_t *idx = ilock.u32();
           unsigned num_vertices = 0;
           unsigned num_indices = 0;
+          vertex circle[PRECISION_CYLINDER];
           
           //Draw circle with the first origin
+          for (unsigned i = 0; i != PRECISION_CYLINDER; ++i){
+  /*FIX*/          //Fill the circle with the basic points of the cylinder
+          }
 
           //Draw circles in given positions
-          //for (unsigned i = 0; i != blocks[cur_iteration].size(); ++i){
-          //  Block *block_ = blocks[cur_iteration][i];
-          //  //Get circle and add the current next
-          //}
+          for (unsigned i = 0; i != blocks[cur_iteration].size(); ++i){
+            Block *block_ = blocks[cur_iteration][i];
+            //Get the bottom and top position of the block and it's orientation
+  /*FIX*/            vec3 pos_1;
+  /*FIX*/            vec3 pos_2;
+  /*FIX*/            vec3 orientation;
+              for (unsigned j = 0; j != PRECISION_CYLINDER; ++j){
+                //Obtain the rotated circle with that orientation
+  /*FIX*/              vec3 pos_c = circle[j].pos; //Rotate with orientation
+  /*FIX*/              vec3 normal_c = circle[j].normal; //Rotate with orientation
+                //Obtain both sides of the cylinder
+  /*FIX*/              vtx->pos = pos_1 + pos_c; vtx->normal = circle[j].normal; vtx->uv = vec2(0, 1);
+                vtx++;
+  /*FIX*/              vtx->pos = pos_2 + pos_c; vtx->normal = circle[j].normal; vtx->uv = vec2(0, 1);
+                vtx++;
+                idx[0] = num_vertices; idx[1] = num_vertices + 1; idx[2] = num_vertices + 3;
+                idx[3] = num_vertices; idx[4] = num_vertices + 3; idx[2] = num_vertices + 2;
+                idx += 6;
+                num_vertices += 2;
+                num_indices += 6;
+              }
+              //Fix last 6 indices to complete the cylinder
+
+            }
+          }
         }
       }
     };
